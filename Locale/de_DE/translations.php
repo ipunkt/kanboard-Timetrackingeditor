e9e11458d183e25cc3eb2b79f444437a35a1c070
Robert Kummer <rok@ipunkt.biz>
2020-02-07 12:06:14 +0100

    translations added

--- Locale/de_DE/translations.php
+++ Locale/de_DE/translations.php
@@ -2,4 +2,7 @@
 
 return array(
 	'Allows Editing of TimeTracking Values' => 'Erlaubt das Bearbeiten von aufgewendeten Zeiten',
+	'billable' => 'abrechenbar',
+	'estimated' => 'geschätzt',
+	'spent' => 'aufgewendet',
 );
